fe025680b3f7ccda1909b18fd8d0ffd244b43326
smcommits <sm.anas@outlook.com>
2021-01-11 20:51:34 +0530

    Create User model, controller, and view.
    - Created a controller for Users with actions new, show, and create
    - Created model for Users
    - Created view for new and show action of Users controller
    - Created a database migration for Users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+end
